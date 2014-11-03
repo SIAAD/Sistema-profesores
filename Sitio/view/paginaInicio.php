85ba29463115804d66656ab7189b68d5775e79a5
Bewtenue <bewtenue@yahoo.com.mx>
2014-11-03 09:33:27 -0600

    act

--- Sitio/view/paginaInicio.php
+++ Sitio/view/paginaInicio.php
@@ -9,7 +9,7 @@
     $roles = $_SESSION['roles'];
 	
 	echo "<h1>BIENVENIDO $codigo</h1><a href='../logout.php'>Cerrar Sesion</a>";
-	echo '<h2>Cargando pagina de inicio personalizada</h2>';
+	echo "<h2>Cargando pagina de inicio personalizada</h2>";
 	var_dump($_SESSION);
 	var_dump($_SERVER);
 
