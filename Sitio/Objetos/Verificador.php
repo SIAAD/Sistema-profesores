f9d8bb0b154856a6c81124f9473cb15b902f572c
Wolfprint <jorge_garza290391@hotmail.com>
2015-01-29 12:32:35 -0600

    primer prueba en el proyecto de funcion verificarParametros

--- Sitio/Objetos/Verificador.php
+++ Sitio/Objetos/Verificador.php
@@ -4,78 +4,88 @@
  * @version 2.0
  */
 class Verificador {
-	
+
 	public function validaNombreMateria($nombrecurso) {//here we validate the syntaxis of the name of the course
-		if(preg_match("/[a-zA-Z ñÑáéíóúâêîôûàèìòùäëïöü]{1,90}$/", $nombrecurso)) return TRUE;
+		if (preg_match("/^[a-zA-Z ñÑáéíóúâêîôûàèìòùäëïöü]{1,90}$/", $nombrecurso))return TRUE;
 		else return FALSE;
-	}
-	
+	}//BIEN
 	public function validaNombreCarrera($nombre) {
-		if(preg_match('/[a-zA-Z]{10,70}$/', $nombre)) return TRUE;
- 		else return FALSE; 
-	}
-	
+		if (preg_match('/^[a-zA-Z]{10,70}$/', $nombre))return TRUE;
+		else return FALSE;
+	}//BIEN
 	public function validaNombreDepartamento($nombre) {
-		if(preg_match('/[a-zA-Z]{10,45}$/', $nombre))return TRUE;
+		if (preg_match('/^[a-zA-Z]{10,45}$/', $nombre))return TRUE;
+		else return FALSE;
+	}//BIEN
+	public function validaNombreAcademia($nombre){
+		if (preg_match('/^[a-zA-Z]{10,70}$/', $nombre))return TRUE;
 		else return FALSE;
 	}
-
 	public function validaSeccion($seccion) {//function to validate the name of the section
-		if (preg_match("/[A-Za-z]+[0-9]+\-D[0-9]+/", $seccion))return TRUE;
+		if (preg_match("/^D[0-9]{2}$/", $seccion))return TRUE;
 		else return FALSE;
-	}
-
+	}//BIEN
 	public function validaNrc($nrc) {//function to validate the nrc of the especific group
-		if (preg_match("/0[0-9]{4}/", $nrc))return TRUE;
+		if (preg_match("/^[0-9]{5}$/", $nrc))return TRUE;
+		else return FALSE;
+	}//BIEN
+	public function validaClaveDepartamento($clave){
+		if (preg_match("/^[0-9]{4}$/", $clave))return TRUE;
+		else return FALSE;
+	}//BIEN
+	public function validaClaveMateria($clave){
+		if (preg_match("/^([A-Z]{1}[0-9]{4})|([A-Z]{2}[0-9]{3})|([A-Z]{3}[0-9]{2})$/", $clave))return TRUE;//et201
+		else return FALSE;
+	}//BIEN
+	public function validaClaveCarrera($clave){
+		if (preg_match("/^([A-Z]){4}$/", $clave))return TRUE;//et201
 		else return FALSE;
 	}
-
-	public function validaSeccion($seccion) {//function to validate the name of the section
-		if (preg_match("/^[A-Za-z]+[0-9]+\-D[0-9]+$/", $seccion))
-			return TRUE;
-		else
-			return FALSE;
-	}
-
-	/*function validaCodigo($codigo) {//Function to validate the code with a lenght of 9 numbers
-	 $codigo = ltrim($codigo);
-	 $codigo = rtrim($codigo);
-	 //We clean the code first
-	 if (preg_match("/^[0-9]{7}/", $codigo))
-	 return TRUE;
-	 else
-	 return FALSE;
-	 }*/
-
+	public function validaHora($hora){
+		if (preg_match("/^[0-9]{4}$/", $hora)){
+			if($hora>=700 && $hora<=2155)return TRUE;
+			else return FALSE;			
+		}
+		else return FALSE;
+	}//BIEN
+	public function validaCodigo($codigo) {
+		if (preg_match("/^[0-9]{7}$/", $codigo))return TRUE;
+		else return FALSE;
+	}//BIEN
+	public function validaFecha($fecha) {//Function to validate the date
+		if (strtotime($fecha) == 0)return FALSE;
+		else return TRUE;
+	}//BIEN
+	public function validaPass($p) {//Function to validate the pass with a lenght of 8-20 characters
+		if (preg_match("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/", $p))return TRUE;
+		else return FALSE;
+	}//BIEN	
+	public function validaAbreviacion($abreviacion) {
+		if (preg_match("/^[A-ZÑ]{3,10}$/", $abreviacion))return TRUE;
+		else return FALSE;
+	}//BIEN
+	public function validaCiclo($ciclo) {
+		if (preg_match("/^((20[0-9]{2}[ABV])|(20[0-9]{2}(10|20|80)))$/", $ciclo))return TRUE;
+		else return FALSE;
+	}//BIEN
+	function validaCorreo($correo) {//Function to validate the syntax of email
+		$correo = ltrim($correo);
+		$correo = rtrim($correo);
+		if (preg_match("/^[a-zA-Z]+[a-zA-Z0-9\._-]{2,}[a-zA-Z0-9]@[a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]+)*$/", $correo))return TRUE;
+		else return FALSE;
+	}//BIEN
 	function validaCadena($cadena) {//Function to validate the syntax of name
 
 		$cadena = ltrim($cadena);
 		$cadena = rtrim($cadena);
-		//We clean the name first
-		if (preg_match("/^[A-Za-z\sñÑáéíóúâêîôûàèìòùäëïöü]+/", $cadena)) return TRUE;
+		if (preg_match("/^[A-Za-z\sñÑáéíóúâêîôûàèìòùäëïöü]+/", $cadena))return TRUE;
 		else return FALSE;
 	}
-
-	function validaCorreo($correo) {//Function to validate the syntax of email
-		$correo = ltrim($correo);
-		$correo = rtrim($correo);
-		//We clean the email first
-		if (preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $correo))return TRUE;
+	function validaNumero($numero){
+		if (preg_match("/^[-]?([0-9]+([.][0-9]+)?|[0][.][0-9]+)$/", $cadena))return TRUE;
 		else return FALSE;
 	}
-
-	public function validaFecha($fecha) {//Function to validate the date
-		if (strtotime($fecha) == 0)return FALSE;
-		else return TRUE;
-
-		//CREAR FECHA DATE(FORMATO,RESULTADO DE FUNCION STRTOTIME)
-	}
-
-	public function validaPass($p) {//Function to validate the pass with a lenght of 6-20 characters
-		if (preg_match("/^[A-Za-z0-9_\-]{6,20}$/", $p))return true;
-		else return FALSE;
-	}
-
+	
 	public function limpiaSQL($variables) {//Posibility to use with the other controllers because is more standard this function
 		foreach ($variables as $llave => $valor) {
 			if (is_string($valor)) {
@@ -87,33 +97,16 @@ class Verificador {
 		return $variables;
 	}
 
-	public function validaCodigo($codigo) {
-		if (preg_match("/^[0-9]{7}$/", $codigo))return true;
-		else return false;
-	}
-
 	public function validaRol($rol) {
-		foreach ($rol as  $value) {
-			if (preg_match("/^[1-5]{1}$/", $value)) return true;
+		foreach ($rol as $value) {
+			if (preg_match("/^[1-5]{1}$/", $value))return true;
 			else return false;
 		}
-	}
 
-	public function validaAbreviacion($abreviacion){
-		if (preg_match("^[A-ZÑ]*$/", $nrc)) return TRUE;
-		else return FALSE;
 	}
-	
-	public function validaNum($numero){
-		if(preg_match("/^[1-9]{1}/",$numero))return TRUE;
+	public function validaNum($numero) {
+		if (preg_match("/^[1-9]{1}/", $numero))return TRUE;
 		else return FALSE;
-	}
-	
-	public function validaEstatus($estatus){
-		if(preg_match("/^[1-3]{1}/",$estatus))
-			return TRUE;
-		else
-			return FALSE;
 	}
 
 }
--- Sitio/ctrl/ctrlStr.php
+++ Sitio/ctrl/ctrlStr.php
@@ -1,122 +1,222 @@
 <?php
 
-	if(!file_exists('../Sitio/Objetos/Verificador.php')){
-		exit();
-	}else{
-		require_once('../Sitio/Objetos/Verificador.php');
+if (!file_exists('../Sitio/Objetos/Verificador.php')) {
+	exit();
+} else {
+	require_once ('../Sitio/Objetos/Verificador.php');
+}
+
+class CtrlStr {
+	protected $modelo;
+	protected $verificador;
+	const ADMIN = 0;
+	const MTRS = 1;
+	const ASIS = 2;
+	const REVIS = 3;
+	const JEFDEP = 4;
+
+	public function __construct() {
+		$this -> verificador = new Verificador();
 	}
 
-    class CtrlStr{
-    	protected $modelo;
-		protected $verificador;
-		const ADMIN = 0;
-		const MTRS = 1;
-		const ASIS = 2;
-		const REVIS = 3;
-		const JEFDEP = 4;
-		
-		public function __construct(){
-			$this->verificador= new Verificador();
-		}
-		
-		public function ejecutar(){
-			
-		}
-		protected function altas($objeto){
-			
-		}
-		protected function bajas($objeto){
-			
-		}
-		protected function consultas($objeto){
-			
-		}
-		protected function modificaciones($objeto){
-			
-		}
-		
-		protected function verificar($variable){
-			if(isset($variable) && !empty($variable) && is_string($variable)) return TRUE;
-			else return FALSE;
-		}	
-		
-		protected function esAdmin($var){
-			if(in_array(CtrlStr::ADMIN,$var)) return TRUE;
-			else return FALSE;
-		}
-		
-		protected function esMstr($var){
-			if(in_array(CtrlStr::MSTR,$var)) return TRUE;
+	public abstract function ejecutar();
+	protected abstract function altas($objeto);
+	protected abstract function bajas($objeto);
+	protected abstract function consultas($objeto);
+	protected abstract function modificaciones($objeto);
+
+	protected function verificar($variable) {
+		if (isset($variable) && !empty($variable) && is_string($variable))return TRUE;
+		else return FALSE;
+	}
+
+	protected function esAdmin($var) {
+		if (in_array(CtrlStr::ADMIN, $var))return TRUE;
+		else return FALSE;
+	}
+
+	protected function esMstr($var) {
+		if (in_array(CtrlStr::MSTR, $var))return TRUE;
+		else return FALSE;
+	}
+
+	protected function esAsis($var) {
+		if (in_array(CtrlStr::ASIS, $var))return TRUE;
+		else return FALSE;
+	}
+
+	protected function esRevis($var) {
+		if (in_array(CtrlStr::REVIS, $var))return TRUE;
+		else return FALSE;
+	}
+
+	protected function esJefDep($var) {
+		if (in_array(CtrlStr::JEFDEP, $var)) return TRUE;
+		else return FALSE;
+	}
+
+	protected function checarAcciones() {
+		if (isset($_GET['accion']) && !empty($_GET['accion'])) {
+			if (isset($_GET['objeto']) && !empty($_GET['objeto']))return TRUE;
 			else return FALSE;
 		}
-		
-		protected function esAsis($var){
-			if(in_array(CtrlStr::ASIS,$var)) return TRUE;
-			else return FALSE;
-		}
-		
-		protected function esRevis($var){
-			if(in_array(CtrlStr::REVIS,$var)) return TRUE;
-			else return FALSE;
-		}
-		
-		protected function esJefDep($var){
-			if(in_array(CtrlStr::JEFDEP,$var)) return TRUE;
-			else return FALSE;
-		}
-		
-		protected function checarAcciones(){
-			if(isset($_GET['accion'])&&!empty($_GET['accion'])){
-				if(isset($_GET['objeto'])&&!empty($_GET['objeto'])){
-					return TRUE;
-				}else{
-				return FALSE;
-				}
+	}
+
+	private function verificarPrivilegios($privilegios) {
+		if (is_array($privilegios)) {
+			$bandera = TRUE;
+			foreach ($privilegios as $privilegio) {
+				$bandera = $bandera and verificarPrivilegio($privilegio);
 			}
+			return $bandera;
+		} else {
+			return $this -> verificarPrivilegio($privilegios);
 		}
-		
-		protected function verificarParametros($variables){
-			$bandera=FALSE;	
-			var_dump($variables);
-			if(is_array($variables)){
-				$keys=array_keys($variables);
-				foreach($keys as $key){
-					var_dump($key);
-					var_dump($variables[$key]);
-					
-					if(!verificar($variables[$key])){
-						return FALSE;
-					}else{
-						switch ($key) {
-							case 'enviar':
-								
-								break;
-								
-							case 'nombreUsuario':
-									if(!$this->verificador->validaCodigo($variables[$key])) return FALSE;
-								break;
-									
-							case 'nombreDepartamento':
-								if(!$this->verificador->validaNombreDepartamento($variables[$key])) return FALSE;
-								break;
-								
-							case 'nombreCarrera':
-								if(!$this->verificador->validaCarrera($variables[$key])) return FALSE;
-								break;
-								
-							case 'nombreMateria':
-								if(!$this->verificador->validaMateria($variables[$key])) return FALSE;
-								break;
-								
-							default:
-								
-								break;
-						}
+	}
+
+	private function verificarPrivilegio($privilegio) {
+		if ($this -> esAdmin($privilegio) || $this -> esAMstr($privilegio) || $this -> esAsis($privilegio) || $this -> esRevis($privilegio) || $this -> esJefDep($privilegio))return TRUE;
+		else return FALSE;
+	}
+
+	protected function verificarParametros($variables) {
+		if (is_array($variables)) {
+			$keys = array_keys($variables);
+			foreach ($keys as $key) {
+				if (!verificar($variables[$key])) {
+					return FALSE;
+				}else{
+					switch ($key) {
+						case 'enviar' :case 'modificar' :case 'valor1' :case 'valor2' :
+							//va ser para enteros utilizados para llaves secundarias
+							break;
+
+						case 'nombreUsuario' :
+							if (!$this->verificador -> validaCodigo($variables[$key]))return FALSE;
+							break;
+
+						case 'nombreDepartamento' :
+							if (!$this->verificador -> validaNombreDepartamento($variables[$key]))return FALSE;
+							break;
+
+						case 'nombreCarreras' :
+							if (!$this->verificador -> validaNombreCarrera($variables[$key]))return FALSE;
+							break;
+
+						case 'nombreMateria' :
+							if (!$this->verificador -> validaNombreMateria($variables[$key]))return FALSE;
+							break;
+
+						case 'nombreAcademia' :
+							if (!$this->verificador -> validaNombreAcademia($variables[$key]))return FALSE;
+							break;
+
+						case 'correo' :
+							if (!$this->verificador -> validaCorreo($variables[$key]))return FALSE;
+							break;
+
+						case 'pass' :
+							if (!$this->verificador -> validaPass($variables[$key]))return FALSE;
+							break;
+
+						case 'tipoPrivilegio' :
+							break;
+
+						case 'descripcionPrivilegio' :
+							break;
+
+						case 'codigoNombramiento' :
+							break;
+
+						case 'nombreNombramiento' :
+							break;
+
+						case 'horasNombramiento' :
+							break;
+
+						case 'estatus' :
+							break;
+
+						case 'descripcionEstatus' :
+							break;
+
+						case 'nombresMaestros' :
+							break;
+
+						case 'codigoMaestros' :
+							if (!$this->verificador -> validaCodigo($variables[$key]))return FALSE;
+							break;
+
+						case 'apellidosMaestros' :
+							break;
+
+						case 'nrc' :
+							if (!$this->verificador -> validaNrc($variables[$key]))return FALSE;
+							break;
+
+						case 'seccion' :
+							if (!$this->verificador -> validaSeccion($variables[$key]))return FALSE;
+							break;
+
+						case 'ciclo' :
+							if (!$this->verificador -> validaCiclo($variables[$key]))return FALSE;
+							break;
+
+						case 'inicioCiclo' :
+						case 'finCiclo' :
+							if (!$this->verificador -> validaFecha($variables[$key]))return FALSE;
+							break;
+
+						case 'claveMateria' :
+							if (!$this->verificador -> validaClaveMateria($variables[$key]))return FALSE;
+							break;
+
+						case 'abreviacionAcademia' :
+						case 'abreviacionDepartamento' :
+							if (!$this->verificador -> validaAbreviacion($variables[$key]))return FALSE;
+							break;
+
+						case 'claveDepartamento' :
+							if (!$this->verificador -> validaClaveDepartamento($variables[$key]))return FALSE;
+							break;
+
+						case 'claveCarreras' :
+							if (!$this->verificador -> validaClaveCarrera($variables[$key]))return FALSE;
+							break;
+
+						case 'aula' :
+							break;
+
+						case 'nombreEdificio' :
+							break;
+
+						case 'diaHorario' :
+							break;
+
+						case 'inicioHorario' :
+						case 'finHorario' :
+							if (!$this->verificador -> validaHora($variables[$key]))return FALSE;
+							break;
+
+						case 'horasHorario' :
+							break;
+
+						case 'teoriaPractica' :
+							break;
+
+						default :
+							echo "No se encontro >>>> " . $key . ">>>>>>" . $variables[$key];
+							exit();
+							break;
 					}
 				}
-			}else{
-				return FALSE;
 			}
+		} else {
+			return FALSE;
 		}
-    }
+		return TRUE;
+
+	}
+
+}
 ?>
