9d1f1f3c7d249670f52bc9b5968add2261140751
Wolfprint <jorge_garza290391@hotmail.com>
2015-01-30 11:09:18 -0600

    expresiones regulares en Verificador arregladas

--- Sitio/view/plantillas/consultaCarreras.php
+++ Sitio/view/plantillas/consultaCarreras.php
@@ -7,12 +7,14 @@ if (file_exists('View/plantillas/PlantillaStr.php')) {
 class ConsultaCarreras extends PlantillaStr {
 	public function generaPagina($res) {
 
-		$contenido = '<table><tr><th>Departamento</th><th>Clave</th><th>Acciones</th></tr>';
+		$contenido = '<table><tr><th>Carrera</th><th>Clave</th><th>Acciones</th></tr>';
 
 		while ($fila = $res -> fetch_assoc()) {
 			$contenido=$contenido.'<tr><td>'.$fila['nombre'].'</td><td>'.$fila['clave'].'</td></tr>';
 		}
-		$contenido=$contenido.'</table>';
+		$contenido=$contenido.'</table></br>';
+		$contenido=$contenido.'<a href="index.php">Vinculo pagina principal</a></br>';
+		$contenido=$contenido.'<a href="index.php?controlador=Estructura&accion=alta&objeto=carrera">Vinculo alta carrera</a>';
 		$res -> free();
 		return $contenido;
 	}
